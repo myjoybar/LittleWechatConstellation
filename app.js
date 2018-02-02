0d45b38f1f60f2e7a8841a9c6cc37e453b9edcb0
joybar <myjoybar@gmail.com>
2018-02-02 15:15:02 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,19 @@
+//app.js
+App({
+  onLaunch: function () {
+    // //调用API从本地缓存中获取数据
+    // var logs = wx.getStorageSync('logs') || []
+    // logs.unshift(Date.now())
+    // wx.setStorageSync('logs', logs)
+
+    console.log('App Launch')
+
+  },
+  onShow: function () {
+    console.log('App Show')
+  },
+  onHide: function () {
+    console.log('App Hide')
+  },
+  
+})
---
+++ page/person/index.js
@@ -0,0 +1,9 @@
+//获取应用实例
+var app = getApp()
+Page({
+ 
+  onLoad: function () {
+    console.log('onLoad')
+   
+  }
+})
